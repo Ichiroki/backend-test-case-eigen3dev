d06923e9c20f7f66550e453f156086fdb314c811
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-09-19 11:50:08 +0700

    soal 3 selesai

--- soal3/script.js
+++ soal3/script.js
@@ -1,4 +1,10 @@
 const input = ['xc', 'dz', 'bbb', 'dz']  
 const query = ['bbb', 'ac', 'dz']
 
-// function 
+function findTheDuplicate(arr1, arr2) {
+    const res = arr2.map((res) => arr1.filter(item => item === res).length)
+
+    console.log(res)
+}
+
+findTheDuplicate(input, query)
