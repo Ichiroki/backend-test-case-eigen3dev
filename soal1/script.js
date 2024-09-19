3252d3a024cf4b622c19a8f642897863209c667a
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-09-19 10:54:41 +0700

    revisi untuk soal no 1

--- soal1/script.js
+++ soal1/script.js
@@ -1,8 +1,25 @@
-const sentence = "Saya sangat senang mengerjakan soal algoritma"
+const sentence = "NEGIE1"
 
 function reverse(txt) {
-    const text = txt.split(" ").reverse().join(" ").toString()
-    console.log(text)
+
+    const number = []
+    const letter = []
+    // const text = txt.split('').reverse().join('')
+
+    for(let i = 0; i < txt.length; i++) {
+        isNaN(txt[i]) ? letter.push(txt[i]) : number.push({char: txt[i], index: i})
+    }
+
+    letter.reverse()
+
+    let res = ''
+    let letterIndex = 0
+
+    for(let i = 0; i < txt.length; i++) {
+        number.length > 0 && number[0].index === i ? res += number.shift().char : res += letter[letterIndex++]
+    }
+
+    console.log(res)
 }
 
 reverse(sentence)
